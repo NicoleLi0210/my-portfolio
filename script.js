1dc21670a7d324623eb597c1a3f14de4bd519680
NicoleLi0210 <sue02102580@gmail.com>
2025-11-23 11:55:55 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// 啟動 AOS (Animate On Scroll) 動畫工具
+// 讓您在 HTML 裡加上 data-aos="動畫名稱" 就能動起來
+AOS.init({
+    duration: 1200, // 動畫持續時間 (毫秒)
+    once: true,     // 只執行一次動畫
+    mirror: false   // 不重複執行動畫
+});
+
+// 您可以在這裡加入更多互動功能，例如：
+// 點擊按鈕顯示更多資訊等等。
